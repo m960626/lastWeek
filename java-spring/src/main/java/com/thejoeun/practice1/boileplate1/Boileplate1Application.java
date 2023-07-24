3f1681f85d4eeb0aad47e39acc032e447a003383
moon <m960626@naver.com>
2023-07-24 10:58:11 +0900

    1. 첫 springboot 세팅

---
+++ java-spring/src/main/java/com/thejoeun/practice1/boileplate1/Boileplate1Application.java
@@ -0,0 +1,13 @@
+package com.thejoeun.practice1.boileplate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boileplate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boileplate1Application.class, args);
+	}
+
+}
